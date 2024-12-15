a0b8e4778b014a11a736fbbf69e732dcc5d2cd14
Robert  St-Pierre <robertstp55@gmail.com>
2024-12-15 10:56:20 -0500

    docker run version

--- project/app.py
+++ project/app.py
@@ -19,9 +19,9 @@ data_load_state = st.text('Loading data...')
 data = load_data(10000)
 data_load_state.text("Done! (using st.cache_data)")
 
-if st.checkbox('Show raw data'):
-    st.subheader('Raw data')
-    st.write(data)
+# if st.checkbox('Show raw data'):
+#     st.subheader('Raw data')
+#     st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
